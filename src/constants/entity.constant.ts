cae0e1bf7ba21b43e6ceafa05b047b40f507f8fa
KhoiMaiDinh <21521008@gm.uit.edu.vn>
2025-03-20 13:18:51 +0700

    feat: add new document consumer (#5)

---
+++ src/constants/entity.constant.ts
@@ -0,0 +1,14 @@
+export enum Entity {
+  MEDIA = 'media',
+  POST = 'post',
+  PREFERENCE = 'preference',
+  PERMISSION = 'permission',
+  ROLE = 'role',
+  USER = 'user',
+  INSTRUCTOR = 'instructor',
+  COURSE = 'course',
+  LESSON = 'lesson',
+  ENROLLED_COURSE = 'enrolled-course',
+  CATEGORY = 'category',
+  CATEGORY_TRANSLATION = 'category-translation',
+}
--- src/consumers/consumer.module.ts
+++ src/consumers/consumer.module.ts
@@ -1,11 +1,16 @@
 import { MediaModule } from '@/api/media';
 import { KafkaModule } from '@/kafka';
 import { Module } from '@nestjs/common';
+import { DocumentProcessConsumer } from './document-process.consumer';
 import { ImageProcessConsumer } from './image-process.consumer';
 import { VideoProcessConsumer } from './video-process.consumer';
 
 @Module({
   imports: [MediaModule, KafkaModule],
-  controllers: [VideoProcessConsumer, ImageProcessConsumer],
+  controllers: [
+    VideoProcessConsumer,
+    ImageProcessConsumer,
+    DocumentProcessConsumer,
+  ],
 })
 export class ConsumerModule {}
