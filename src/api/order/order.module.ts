c212313d83ff6a1a7c80e908000568685e3f626c
KhoiMaiDinh <21521008@gm.uit.edu.vn>
2025-04-16 23:08:44 +0700

    refactor: using repo from user module (#8)

--- src/api/order/order.module.ts
+++ src/api/order/order.module.ts
@@ -1,22 +1,18 @@
 import { PaymentModule } from '@/api/payment/payment.module';
+import { UserModule } from '@/api/user/user.module';
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseModule } from '../course/course.module';
 import { CourseEntity } from '../course/entities/course.entity';
 import { OrderController } from '../order/order.controller';
-import { UserEntity } from '../user/entities/user.entity';
 import { OrderDetailEntity } from './entities/order-detail.entity';
 import { OrderEntity } from './entities/order.entity';
 import { OrderService } from './services/order.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      OrderEntity,
-      OrderDetailEntity,
-      CourseEntity,
-      UserEntity,
-    ]),
+    UserModule,
+    TypeOrmModule.forFeature([OrderEntity, OrderDetailEntity, CourseEntity]),
     forwardRef(() => PaymentModule.forRootAsync()),
     CourseModule,
   ],
