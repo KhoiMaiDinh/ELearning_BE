6eb6c9c3e1dc594fcfd67ebdec19e31caf2568f8
KhoiMaiDinh <21521008@gm.uit.edu.vn>
2025-04-28 02:27:02 +0700

    feat: lecture comment module (#12)

--- src/api/lecture-comment/dto/create-comment.req.dto.ts
+++ src/api/lecture-comment/dto/create-comment.req.dto.ts
@@ -2,7 +2,6 @@ import { Nanoid } from '@/common';
 import { StringField } from '@/decorators';
 
 export class CreateCommentReq {
-  @StringField()
   lecture_id: Nanoid;
 
   @StringField()
--- src/consumers/consumer.module.ts
+++ src/consumers/consumer.module.ts
@@ -1,16 +1,19 @@
+import { LectureCommentModule } from '@/api/lecture-comment/lecture-comment.module';
 import { MediaModule } from '@/api/media';
 import { KafkaModule } from '@/kafka';
 import { Module } from '@nestjs/common';
+import { CommentProcessConsumer } from './comment-process.consumer';
 import { DocumentProcessConsumer } from './document-process.consumer';
 import { ImageProcessConsumer } from './image-process.consumer';
 import { VideoProcessConsumer } from './video-process.consumer';
 
 @Module({
-  imports: [MediaModule, KafkaModule],
+  imports: [MediaModule, KafkaModule, LectureCommentModule],
   controllers: [
     VideoProcessConsumer,
     ImageProcessConsumer,
     DocumentProcessConsumer,
+    CommentProcessConsumer,
   ],
 })
 export class ConsumerModule {}
