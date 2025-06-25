c5ba4c3ff1e067df3e75aaa1cd06aa32009a79cf
KhoiMaiDinh <21521008@gm.uit.edu.vn>
2025-06-25 18:25:21 +0700

    refactor: improve error handling and code organization (#14)

--- src/api/recommender/recommender.module.ts
+++ src/api/recommender/recommender.module.ts
@@ -1,9 +1,9 @@
+import { UserModule } from '@/api/user/user.module';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { CategoryModule } from '../category/category.module';
 import { CourseModule } from '../course/course.module';
 import { PreferenceModule } from '../preference/preference.module';
-import { UserModule } from '../user';
 import { RecommenderController } from './recommender.controller';
 import { RecommenderService } from './recommender.service';
 
