0020b79a04aea4add12e74e4eafed11481b3308e
KhoiMaiDinh <21521008@gm.uit.edu.vn>
2025-08-17 23:11:35 +0700

    feat: add warning repository and enhance warning service (#14)

--- src/api/ban/services/warning.service.ts
+++ src/api/ban/services/warning.service.ts
@@ -2,21 +2,24 @@ import { CouponService } from '@/api/coupon/coupon.service';
 import { CouponType } from '@/api/coupon/enum/coupon-type.enum';
 import { CourseRepository, CourseStatus } from '@/api/course';
 import { LectureRepository } from '@/api/course-item/lecture/repositories/lecture.repository';
+import { CourseEntity } from '@/api/course/entities/course.entity';
+import { NotificationType } from '@/api/notification/enum/notification-type.enum';
+import { NotificationBuilderService } from '@/api/notification/notification-builder.service';
+import { NotificationService } from '@/api/notification/notification.service';
 import { ReplyRepository } from '@/api/thread/repositories/reply.repository';
 import { ThreadRepository } from '@/api/thread/repositories/thread.repository';
 import { JwtPayloadType } from '@/api/token';
 import { UserEntity } from '@/api/user/entities/user.entity';
-import { UserRepository } from '@/api/user/user.repository';
 import { IGiveCouponJob, Nanoid, Uuid } from '@/common';
 import { JobName, QueueName } from '@/constants';
+import { NotificationGateway } from '@/gateway/notification/notification.gateway';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Queue } from 'bullmq';
-import { Repository } from 'typeorm';
 import { UserReportEntity } from '../entities/user-report.entity';
 import { WarningEntity } from '../entities/warning.entity';
 import { WarningType } from '../enum/warning-type.enum';
+import { WarningRepository } from '../repositories/warning.repository';
 import { UserBanService } from './ban.service';
 
 @Injectable()
@@ -24,18 +27,21 @@ export class WarningService {
   private readonly WARNING_LIMIT = 1;
 
   constructor(
-    @InjectRepository(WarningEntity)
-    private readonly warningRepo: Repository<WarningEntity>,
     private readonly userBanService: UserBanService,
 
+    private readonly warningRepo: WarningRepository,
     private readonly replyRepo: ReplyRepository,
     private readonly threadRepo: ThreadRepository,
     private readonly courseRepo: CourseRepository,
     private readonly lectureRepo: LectureRepository,
-    private readonly userRepo: UserRepository,
+
     @InjectQueue(QueueName.EMAIL)
     private readonly emailQueue: Queue<IGiveCouponJob, any, string>,
     private readonly couponService: CouponService,
+
+    private readonly notificationService: NotificationService,
+    private readonly notificationBuilder: NotificationBuilderService,
+    private readonly notificationGateway: NotificationGateway,
   ) {}
 
   async issueWarning(
@@ -68,7 +74,10 @@ export class WarningService {
           where: { id: report.metadata.lecture_id },
           relations: {
             section: {
-              course: { enrolled_users: { user: true } },
+              course: {
+                enrolled_users: { user: true },
+                instructor: { user: true },
+              },
             },
             series: true,
           },
@@ -83,8 +92,8 @@ export class WarningService {
 
         const ONE_YEAR_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 365;
 
-        await Promise.all(
-          course.enrolled_users.map(async (enroll) => {
+        const enrolled_user_promises = course.enrolled_users.map(
+          async (enroll) => {
             const coupon = await this.couponService.create(user_payload, {
               type: CouponType.PERCENTAGE,
               value: 100,
@@ -93,18 +102,28 @@ export class WarningService {
               usage_limit: 1,
               is_public: false,
             });
-            await this.emailQueue.add(
+
+            return this.emailQueue.add(
               JobName.COUPON_GIFT,
               {
                 email: enroll.user.email,
                 coupon_code: coupon.code,
-                reason:
-                  'We recently reviewed your purchase and found that the course you enrolled in has violated our platform’s terms and conditions. We understand this may have caused inconvenience.',
+                reason: `Chúng tôi vừa xem xét giao dịch mua của bạn và phát hiện khóa học bạn đã đăng ký vi phạm điều khoản và điều kiện của nền tảng. Chúng tôi hiểu điều này có thể gây bất tiện cho bạn.`,
               } as IGiveCouponJob,
               { attempts: 3, backoff: { type: 'exponential', delay: 60000 } },
             );
-          }),
+          },
         );
+
+        const banned_notification_promise = this.sendBannedNotification(
+          lecture.section.course.instructor.user,
+          course,
+        );
+
+        await Promise.all([
+          ...enrolled_user_promises,
+          banned_notification_promise,
+        ]);
         break;
       }
     }
@@ -128,6 +147,22 @@ export class WarningService {
     }
 
     return warning;
+  }
+
+  private async sendBannedNotification(user: UserEntity, course: CourseEntity) {
+    const built_notification = this.notificationBuilder.courseBanned(course);
+    const notification = await this.notificationService.save(
+      user.user_id,
+      NotificationType.COURSE_BANNED,
+      {
+        course_id: course.id,
+      },
+      built_notification,
+    );
+    this.notificationGateway.emitToUser(user.id, {
+      ...notification,
+      ...built_notification,
+    });
   }
 
   async getActiveWarnings(user_id: Nanoid): Promise<WarningEntity[]> {
