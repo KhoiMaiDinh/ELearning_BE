b27ae58dbaeadf561fac776af9f0a5507da05062
KhoiMaiDinh <21521008@gm.uit.edu.vn>
2025-08-17 19:00:09 +0700

    feat(payout): update payout schedule and add finalize job

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,12 +1,13 @@
 import { OrderDetailService } from '@/api/order/services/order-detail.service';
-import { IPayoutJob } from '@/common';
+import { IPayoutFinalizeJob, IPayoutJob } from '@/common';
 import { JobName, QueueName } from '@/constants';
 import { Redlock } from '@anchan828/nest-redlock';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { Queue } from 'bullmq';
 
+const PAYOUT_CRON_SCHEDULE = '0 0 27 * *';
 @Injectable()
 export class TaskService {
   private readonly logger = new Logger(TaskService.name);
@@ -14,15 +15,19 @@ export class TaskService {
   constructor(
     private readonly orderDetailService: OrderDetailService,
     @InjectQueue(QueueName.PAYOUT)
-    private readonly stripeQueue: Queue<IPayoutJob, any, string>,
+    private readonly payoutQueue: Queue<
+      IPayoutJob | IPayoutFinalizeJob,
+      any,
+      string
+    >,
   ) {}
 
-  @Cron(CronExpression.EVERY_10_MINUTES)
+  @Cron(PAYOUT_CRON_SCHEDULE)
   @Redlock('payout-instructors')
   async payoutInstructors() {
     this.logger.debug('Payout Instructors');
     const payee_ids = await this.orderDetailService.findPayableInstructor();
-    await this.stripeQueue.addBulk(
+    await this.payoutQueue.addBulk(
       payee_ids.map((instructor_id) => ({
         name: JobName.PAYOUT_INSTRUCTOR,
         data: {
@@ -30,5 +35,9 @@ export class TaskService {
         },
       })),
     );
+    await this.payoutQueue.add(JobName.PAYOUT_FINALIZE, {
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear(),
+    } as IPayoutFinalizeJob);
   }
 }
